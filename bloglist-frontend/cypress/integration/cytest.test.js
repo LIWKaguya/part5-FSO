645ad25b932668445453aa1410089f7218125296
LIWKaguya <hungle120901@gmail.com>
2021-11-25 00:22:59 +0200

    Exercise 5.17

---
+++ bloglist-frontend/cypress/integration/cytest.test.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+describe('Blog app', () => {
+    beforeEach(() => {
+        cy.request('POST', 'http://localhost:3003/api/testing/reset')
+        const user = {
+          name: 'name1',
+          username: 'username1',
+          password: 'password1'
+        }
+        cy.request('POST', 'http://localhost:3003/api/users/', user)
+        cy.visit('http://localhost:3000')
+      })
+    
+    it('Login form is shown', () => {
+        cy.contains('login').click()
+        cy.get('#username').type('username1')
+        cy.get('#password').type('password1')
+        cy.get('#login-button').click()
+
+        cy.contains('username1 logged in')
+    })
+})
