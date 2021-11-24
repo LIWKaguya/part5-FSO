b0d740c4fb4db8e8eb807aa3b6039354205c930c
LIWKaguya <hungle120901@gmail.com>
2021-11-24 15:34:07 +0200

    Exercise 5.15

--- bloglist-frontend/src/components/Blog_test.test.js
+++ bloglist-frontend/src/components/Blog_test.test.js
@@ -1,11 +1,11 @@
 import { React } from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent, screen } from '@testing-library/react'
 import Togglable from './Togglable'
 import Blog from './Blog'
 
 describe('Frontend test', () => {
-  let component
+  let component, mockUpdate, mockDelete
 
   beforeEach(() => {
     let blog = {
@@ -20,8 +20,8 @@ describe('Frontend test', () => {
     let currentUser = {
       username: 'username1'
     }
-    let mockUpdate = jest.fn()
-    let mockDelete = jest.fn()
+    mockUpdate = jest.fn()
+    mockDelete = jest.fn()
     component = render(
       <Togglable buttonLabel='show....' cancelLabel='cancel'>
         <Blog blog={blog} updateBlog={mockUpdate} deleteBlog={mockDelete} currentUser={currentUser}/>
@@ -40,5 +40,14 @@ describe('Frontend test', () => {
     const togg = component.container.querySelector('.togglableContent')
     expect(togg).toHaveTextContent('www.test.com')
     expect(togg).toHaveTextContent('69')
+  })
+
+  test('like button clicked twice', () => {
+    const show = component.getByText('show....')
+    fireEvent.click(show)
+    const likeButton = component.getByText('Like')
+    fireEvent.click(likeButton)
+    fireEvent.click(likeButton)
+    expect(mockUpdate.mock.calls).toHaveLength(2)
   })
 })
